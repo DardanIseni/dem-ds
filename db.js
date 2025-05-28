ea053fcd5598e3fa9cf1100ff65f91b06825975b
dardan <isenidardan@hotmail.com>
2025-05-29 01:55:30 +0200

    minor change

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const data = [
+    {
+        "id":1,
+        "name":"Dardan",
+        "email":"test@test.com"
+    },
+    {
+        "id":2,
+        "name":"test",
+        "email":"test@test.com"
+    }
+]
 
-const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-});
-
-module.exports = pool;
+module.exports = data;
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const data = require('../db');
 
-// GET all users
+// GET users
 router.get('/', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM users');
-        res.json(result.rows);
+        res.json(data);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
 
-// POST new user
-router.post('/', async (req, res) => {
-    const { name, email } = req.body;
-    try {
-        await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
-        res.status(201).json({ message: 'User added' });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-});
 
 module.exports = router;
